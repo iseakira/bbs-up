69b591f5106b957bb4eeee2dc26c9b9a0c4d02dd
IseAkira <akira.ise.m@gmail.com>
2026-03-27 13:28:05 +0900

    serveraction追加ログインに関して

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,56 +1,69 @@
+'use client';
+import { useState } from 'react';
+import { signup } from '@actions/auth';
 import Link from 'next/link';
 
 export default function SignupPage() {
+  const [error, setError] = useState<string | null>(null);
+  const handleSubmit = async (formData: FormData) => {
+    setError(null);
+    const result = await signup(formData);
+
+    if (result && result.error) {
+      setError(result.error);
+    }
+  };
+
   return (
-    <div className='container' style={{ maxWidth: '400px', marginTop: '50px' }}>
-      <div className='card'>
+    <div className="container" style={{ maxWidth: '400px', marginTop: '50px' }}>
+      <div className="card">
         <h2 style={{ marginBottom: '20px', textAlign: 'center' }}>
           アカウント登録
         </h2>
-        <form>
-          <div className='form-group'>
-            <label className='form-label' htmlFor='username'>
+        <form action={handleSubmit}>
+          <div className="form-group">
+            <label className="form-label" htmlFor="username">
               ユーザー名
             </label>
             <input
-              type='text'
-              id='username'
-              name='username'
-              className='form-input'
-              placeholder='ユーザー名'
+              type="text"
+              id="username"
+              name="username"
+              className="form-input"
+              placeholder="ユーザー名"
               required
             />
           </div>
-          <div className='form-group'>
-            <label className='form-label' htmlFor='email'>
+          <div className="form-group">
+            <label className="form-label" htmlFor="email">
               メールアドレス
             </label>
             <input
-              type='email'
-              id='email'
-              name='email'
-              className='form-input'
-              placeholder='example@email.com'
+              type="email"
+              id="email"
+              name="email"
+              className="form-input"
+              placeholder="example@email.com"
               required
             />
           </div>
-          <div className='form-group'>
-            <label className='form-label' htmlFor='password'>
+          <div className="form-group">
+            <label className="form-label" htmlFor="password">
               パスワード
             </label>
             <input
-              type='password'
-              id='password'
-              name='password'
-              className='form-input'
-              placeholder='********'
+              type="password"
+              id="password"
+              name="password"
+              className="form-input"
+              placeholder="********"
               required
             />
           </div>
-          {/* {error && <p className='error-message'>{error}</p>} */}
+          {error && <p className="error-message">{error}</p>}
           <button
-            type='submit'
-            className='btn'
+            type="submit"
+            className="btn"
             style={{ width: '100%', marginBottom: '15px' }}
           >
             登録する
@@ -58,7 +71,7 @@ export default function SignupPage() {
         </form>
         <p style={{ textAlign: 'center', fontSize: '14px' }}>
           すでにアカウントをお持ちですか？ <br />
-          <Link href='#' style={{ color: '#0070f3' }}>
+          <Link href="\/login" style={{ color: '#0070f3' }}>
             ログインはこちら
           </Link>
         </p>
