117ff8dc29aed45296ea87f59346f34423757dfe
IseAkira <akira.ise.m@gmail.com>
2026-03-27 13:35:05 +0900

    ユーザー登録機能作成

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { signup } from '@actions/auth';
+import { signup } from '../../actions/auth';
 import Link from 'next/link';
 
 export default function SignupPage() {
